1f4408798d19bb4a27dcdbb4cfd93ef4752adcb3
Monish soni <monishsoni3@gmail.com>
2025-01-22 20:02:28 +0530

    product

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,7 +4,7 @@ const config = require('config');
 const dbgr = require('debug')("development:mongoose");
 
 
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+mongoose.connect(config.get("MONGODB_URI"))
 .then(function(){
     dbgr('connected to db');
 })
